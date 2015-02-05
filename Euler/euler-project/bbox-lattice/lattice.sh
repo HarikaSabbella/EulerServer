ad65da350e070de3eb12e17a8deb3697352aac2c
HarikaSabbella <HarikaSabbella@users.noreply.github.com>
2015-02-05 12:09:00 -0800

    Update lattice.sh

--- Euler/euler-project/bbox-lattice/lattice.sh
+++ Euler/euler-project/bbox-lattice/lattice.sh
@@ -5,7 +5,7 @@ python /var/www/html/Euler/euler-project/bbox-lattice/awf.py i,o /var/www/html/E
 echo "creating the lattice without color...";
 /var/www/html/Euler/binary/dlv -silent -filter=up  /var/www/html/Euler/euler-project/bbox-lattice/wexp-up.asp /var/www/html/Euler/euler-project/bbox-lattice/expWorlds_aw.asp > /var/www/html/Euler/euler-project/bbox-lattice/up.dlv;
 echo "running euler to get MIS...";
-#/var/www/html/Euler/euler-project/src-el/euler -i $1.txt -e mnpw --ie > /var/www/html/Euler/euler-project/bbox-lattice/output.txt;
+/var/www/html/Euler/euler-project/src-el/euler -i $1.txt -e mnpw --ie > /var/www/html/Euler/euler-project/bbox-lattice/output.txt;
 echo "from MIS to MAC and get lattice..."
 python /var/www/html/Euler/euler-project/bbox-lattice/lattice.py $1;
 python /var/www/html/Euler/euler-project/bbox-lattice/latticedot2yaml.py $1;
